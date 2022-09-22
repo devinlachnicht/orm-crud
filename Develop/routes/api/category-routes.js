be9e38a262ee69b55f99ad3a565169e69586b957
devin <devin@devins-MacBook-Pro.local>
2022-09-22 05:49:09 -0400

    updated tags routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -91,7 +91,7 @@ router.delete('/:id', (req, res) => {
   })
     .then(dbCategoryData => {
       if (!dbCategoryData) {
-        res.status(404).json({ message: 'No category found with this id' });
+        res.status(404).json({ message: 'No existing category with this id' });
         return;
     }
     res.json(dbCategoryData);
